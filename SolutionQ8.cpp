df066ec8f889fbed0d3c3c2587eca05736293035
Harsh Rajbhar <119682671+VadaPavMan@users.noreply.github.com>
2024-11-19 16:59:47 +0530

    Create SolutionQ8.cpp

---
+++ SolutionQ8.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <string>
+#include <limits>
+#include <conio.h>
+using namespace std;
+
+class shape
+{
+protected:
+    double width, height;
+
+public:
+    void setdimension(double wid, double hei)
+    {
+        width = wid;
+        height = hei;
+    }
+};
+
+class rectangle : public shape
+{
+public:
+    double area;
+    void calculatearea()
+    {
+        area = width * height;
+        cout << "This is the area: " << area << endl;
+    }
+};
+
+class cuboid : public rectangle
+{
+public:
+    void calvolume(double depth)
+    {
+        cout << "Volume of cuboid: " << area * depth << endl;
+    }
+};
+
+int main()
+{
+    double depth;
+    cout << "Enter the depth: ";
+    cin >> depth;
+
+    cuboid land;
+    land.setdimension(44.2, 22.3);
+    land.calculatearea();
+    land.calvolume(depth);
+
+    return 0;
+}
