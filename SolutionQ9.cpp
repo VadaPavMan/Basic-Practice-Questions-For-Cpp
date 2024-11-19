e22bee44ea9a545dc88996437b906b010886a228
Harsh Rajbhar <119682671+VadaPavMan@users.noreply.github.com>
2024-11-19 17:21:48 +0530

    Create SolutionQ9.cpp

---
+++ SolutionQ9.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <string>
+#include <limits>
+#include <conio.h>
+using namespace std;
+
+class person
+{
+    public:
+    string name;
+    int age;
+
+    void displaypersoninfo()
+    {
+        cout <<"Name: "<< name << endl;
+        cout <<"Age: "<< age << endl;
+    }
+};
+
+class student: public person
+{
+    public: 
+    int rollnumber;
+
+    void displaystudentinfo()
+    {
+        cout<<"Roll Number: "<< rollnumber << endl;
+    }
+};
+
+class graduatestudent: public student
+{
+    public:
+    string specialization;
+    void dispalaygraduateinfo()
+    {
+        cout <<"Graduated from: "<< specialization << endl;
+    }
+};
+int main()
+{
+    graduatestudent st;
+    st.age = 69;
+    st.name = "Harsh";
+    st.rollnumber = 57;
+    st.specialization = "BCA";
+    st.displaypersoninfo();
+    st.displaystudentinfo();
+    st.dispalaygraduateinfo();
+    return 0;
+}
